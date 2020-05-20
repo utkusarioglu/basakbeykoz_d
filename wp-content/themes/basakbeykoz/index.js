e8637148642ace3bacd95358b6aa3e5c664a68e1
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-05-20 12:29:01 +0300

    Init react

--- wp-content/themes/basakbeykoz/index.js
+++ wp-content/themes/basakbeykoz/index.js
@@ -1,5 +1,6 @@
-fetch("/wp-json/wp/v2/posts")
+fetch("/wp-json/wp/v2/pages/3")
     .then((data) => data.json())
     .then((json) => {
-        document.body.innerHTML = JSON.stringify(json)
+        document.body.innerHTML = json.content.rendered
+        console.log(json);
     });
