1e24ced119e38bde62bed0f462228cd5d740a0f4
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-06-13 12:25:50 +0300

    Comment categoryActions.ts
    Category actions still uses WPAPI which has been removed
    with the WPAPI_remove branch. This class has been completely
    disabled as its use in this project is unlikely

--- themes/basakbeykoz/react-src/src/wp/categoryActions.ts
+++ themes/basakbeykoz/react-src/src/wp/categoryActions.ts
@@ -2,44 +2,44 @@ import { DispatchMethod } from '../common/@types-actions';
 
 import { FETCH_CATEGORY, FETCH_CATEGORIES } from '../common/types'
 
-import WPAPI, { WPRequest } from "wpapi";
+// import WPAPI, { WPRequest } from "wpapi";
 
-const wp = new WPAPI({endpoint: "?rest_route=/"})
+// const wp = new WPAPI({endpoint: "?rest_route=/"})
 
-export const fetchCategories = () => (dispatch: DispatchMethod<any>) => { //!HACK any
-    wp.categories()
-        .get((err: Error, data: any) => {
-            if(err) {
-                dispatch({
-                    type: FETCH_CATEGORIES,
-                    state: "fail", 
-                    error: "somehting went wrong", //TODO
-                });
-            } else {
-                dispatch({
-                    type: FETCH_CATEGORIES,
-                    state: "success",
-                    payload: Object.values(data).slice(0,-1),
-                })
-            }
-        });
-}
+// export const fetchCategories = () => (dispatch: DispatchMethod<any>) => { //!HACK any
+//     wp.categories()
+//         .get((err: Error, data: any) => {
+//             if(err) {
+//                 dispatch({
+//                     type: FETCH_CATEGORIES,
+//                     state: "fail", 
+//                     error: "somehting went wrong", //TODO
+//                 });
+//             } else {
+//                 dispatch({
+//                     type: FETCH_CATEGORIES,
+//                     state: "success",
+//                     payload: Object.values(data).slice(0,-1),
+//                 })
+//             }
+//         });
+// }
 
-export const fetchCategory = (id: number) => (dispatch: DispatchMethod<any>) => { //!HACK any
-    (wp.categories().id(id) as WPRequest)
-        .get((err: Error, data: any) => {
-            if(err) {
-                dispatch({
-                    type: FETCH_CATEGORY,
-                    state: "fail",
-                    error: "something went wrong",
-                });
-            } else {
-                dispatch({
-                    type: FETCH_CATEGORY,
-                    state: "success",
-                    payload: data
-                })
-            }
-        });
-}
+// export const fetchCategory = (id: number) => (dispatch: DispatchMethod<any>) => { //!HACK any
+//     (wp.categories().id(id) as WPRequest)
+//         .get((err: Error, data: any) => {
+//             if(err) {
+//                 dispatch({
+//                     type: FETCH_CATEGORY,
+//                     state: "fail",
+//                     error: "something went wrong",
+//                 });
+//             } else {
+//                 dispatch({
+//                     type: FETCH_CATEGORY,
+//                     state: "success",
+//                     payload: data
+//                 })
+//             }
+//         });
+// }
