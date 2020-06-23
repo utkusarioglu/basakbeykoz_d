83d68ef8383fcffedab88812a65222dd34612863
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-06-23 11:57:08 +0300

    Edit Navigaton, add current page ind, fix undefined slug
    Navigation menu now completely relies on sass for styling, there
    are no inline styles.
    
    Also, there was an issue with the home page creating an undefined
    slug as wp does not return a slug when homepage is called. This
    has been fixed with the slug being run through an undefined
    value check before being assigned to the link.
    
    The mobile navigation menu now has a burger menu icon

--- themes/basakbeykoz/react-src/src/header/Nav.tsx
+++ themes/basakbeykoz/react-src/src/header/Nav.tsx
@@ -5,7 +5,7 @@ import { fetchMenu } from "../wp/menuActions";
 // import { fetchPost, fetchCategoryPosts } from '../wp/postActions';
 import { setDisplaying } from '../app/appActions'
 // import { fetchSingular } from "../wp/singularActions";
-import CSS from 'csstype';
+// import CSS from 'csstype';
 import { wpMenuItem } from '../wp/@types-wp';
 import { RootState } from '../app/rootReducer';
 import "./_header_nav.scss"
@@ -30,39 +30,41 @@ type DispatchProps = typeof mapDispatch;
 type StateProps = ReturnType<typeof mapState>;
 type Props = DispatchProps & StateProps & OwnProps;
 
-// TODO better static typing for style elements
-const styles: { [className: string]: CSS.Properties} = {
-    // navDecor: {
-    //     width: "var(--accent-thickness)",
-    //     height: "100%",
-    //     background: "var(--yellow)",
-    //     position: "absolute",
-    //     right: 0,
-    // },
-    // navWrap: {
-    //     padding: "5px",
-    //     position: "fixed",
-    //     right: 0,
-    //     top: "50%",
-    //     transform: "translateY(-50%)",
-    //     zIndex: 10,
-    // },
-    // navList: {
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     padding: "30px 30px 30px 0",
-    // },
-    // navItem: {
-    //     padding: "10px 15px",
-    //     textDecoration: "none",
-    //     fontSize: "24px",
-    //     textAlign: "right",
-    //     color: "var(--blue)",
-    // },
-    navItemActive: {
-        color: "red"
-    }
-}
+// // TODO better static typing for style elements
+// const styles: { [className: string]: CSS.Properties} = {
+//     // navDecor: {
+//     //     width: "var(--accent-thickness)",
+//     //     height: "100%",
+//     //     background: "var(--yellow)",
+//     //     position: "absolute",
+//     //     right: 0,
+//     // },
+//     // navWrap: {
+//     //     padding: "5px",
+//     //     position: "fixed",
+//     //     right: 0,
+//     //     top: "50%",
+//     //     transform: "translateY(-50%)",
+//     //     zIndex: 10,
+//     // },
+//     // navList: {
+//     //     display: "flex",
+//     //     flexDirection: "column",
+//     //     padding: "30px 30px 30px 0",
+//     // },
+//     // navItem: {
+//     //     padding: "10px 15px",
+//     //     textDecoration: "none",
+//     //     fontSize: "24px",
+//     //     textAlign: "right",
+//     //     color: "var(--blue)",
+//     // },
+//     // navItemActive: {
+
+//     //     textDecoration: "underline",
+//     //     // color: "red"
+//     // }
+// }
 
 function Nav(
     props: Props
@@ -82,24 +84,25 @@ function Nav(
         });        
     }
 
-    // let menuIsOpen = false;
     const menuToggle = () => {
-        // alert("yeh")
         setMenuIsOpen((menuIsOpen) => !menuIsOpen);
-        // menuIsOpen = !menuIsOpen;
     }
 
     const menuItems: JSX.Element[] = props.menu.items
         .map((item: wpMenuItem) => {
+
+            const clean_slug = item.slug !== undefined
+                ? item.slug
+                : "";
+
             return (
                 <NavLink 
                     key={item.ID}
                     exact
-                    to={"/" + item.slug}
+                    to={"/" + clean_slug}
                     className="Nav-list-item"
-                    // style={styles.navItem}
-                    activeStyle={styles.navItemActive}
-                    onClick={() => setDisplaying(item.slug)}
+                    activeClassName="Nav-list-item-active"
+                    onClick={() => setDisplaying(clean_slug)}
                     >{item.title}
                 </NavLink>
             )
