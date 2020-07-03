2f3e7e10e01f104fa806caab39bfe61e09c893e5
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-07-03 11:35:13 +0300

    Comment create_image.sh

--- create_image.sh
+++ create_image.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#prepare react production build
 cd ./themes/basakbeykoz/react-src
 sudo yarn build
+
+# move theme's dev copy out of the way
 cd ../../..
 sudo mv ./themes/basakbeykoz ./basakbeykoz-dev
+
+#place production build in the themes folder
 sudo mv ./basakbeykoz-dev/basakbeykoz ./themes/basakbeykoz
 docker build -t utkusarioglu/basakbeykoz .
+
+#remove dev folder and rename production folder for its place
 sudo rm -rf ./themes/basakbeykoz
 sudo mv ./basakbeykoz-dev ./themes/basakbeykoz
