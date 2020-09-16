520373ed9164978d42460763e8fd73d61f2ee7bb
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-16 20:32:33 +0300

    Refactor host_init for db
    The container name has been converted into a variable to make it clear
    that this value changes in production

--- scripts/basakbeykoz_db_d/host_init.sh
+++ scripts/basakbeykoz_db_d/host_init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #this line is a curl command in production that gets the ip of the vm
 EXTERNAL_IP="http://localhost"
-docker exec -it basakbeykoz_db_p sh -c "bash /scripts/container_init.sh $EXTERNAL_IP"
+CONTAINER_NAME="basakbeykoz_db_d"
+docker exec -it $CONTAINER_NAME sh -c "bash /scripts/container_init.sh $EXTERNAL_IP"
