4d0a5c311abb05cbcde8bf31a9580fcec023d320
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-20 10:28:24 +0300

    Add sleep to init

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@
 docker-compose up -d
 sudo rm -rf ./themes/twenty* ./themes/index* 
 sudo rm -rf ./plugins/akis* ./plugins/index* ./plugins/hello*
+#sleep can later be replaced by a better command later
+sleep 20
 bash ./scripts/basakbeykoz_db_d/host_init.sh 192.168.1.152
 (cd ./themes/basakbeykoz/react-src && sudo yarn start)
