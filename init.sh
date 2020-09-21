88092b1f7b34be50d67882bb2bcbdcc698d7a656
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-21 21:08:50 +0300

    Add logic for determining eth0 ip

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ sudo rm -rf ./themes/twenty* ./themes/index*
 sudo rm -rf ./plugins/akis* ./plugins/index* ./plugins/hello*
 #sleep can later be replaced by a better command later
 sleep 20
-bash ./scripts/basakbeykoz_db_d/host_init.sh 192.168.1.152
+bash ./scripts/basakbeykoz_db_d/host_init.sh
 (cd ./themes/basakbeykoz/react-src && sudo yarn start)
