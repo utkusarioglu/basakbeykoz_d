b06661d16e57300908f005e587ad2331cb49539b
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-22 14:01:46 +0300

    Refactor social items

--- themes/basakbeykoz/react-src/src/components/social/SocialItem.tsx
+++ themes/basakbeykoz/react-src/src/components/social/SocialItem.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { ISocialItem } from '../../features/social/@types-social';
+import {setIsMenuOpen} from '../../app/appActions';
 import ReactGA from "react-ga";
+import { RootState } from "../../app/rootReducer";
 import './_socialIcon.scss';
 
 interface OwnProps {
     item: ISocialItem;
 }
 
-type Props = OwnProps;
+const mapState = (state: RootState) => ({
+});
+
+const mapDispatch = {
+    setIsMenuOpen,
+}
+
+interface OwnProps {}
+type DispatchProps = typeof mapDispatch;
+type StateProps = ReturnType<typeof mapState>;
+type Props = DispatchProps & StateProps & OwnProps;
 
 function SocialItem(props: Props) {
 
-    const { item } = props;
+    const { item, setIsMenuOpen } = props;
 
     return (
         <ReactGA.OutboundLink
             eventLabel={item.title}
+            onClick={() => setIsMenuOpen(false) }
             to={item.link}
             target="_blank"
         >
@@ -29,4 +43,7 @@ function SocialItem(props: Props) {
     )
 }
 
-export default SocialItem;
+
+export default connect<StateProps, DispatchProps, OwnProps>(
+    //@ts-ignore
+    mapState, mapDispatch, )(SocialItem);
