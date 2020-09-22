4d3babbee20058402e1d99f76495a66fabc8c44c
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-22 14:57:23 +0300

    Implement routes

---
+++ themes/basakbeykoz/react-src/src/routes/BlogRoute.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Blog from '../blog/Blog';
+
+function BlogRoute() {
+    return <Blog />
+}
+
+export default BlogRoute;
---
+++ themes/basakbeykoz/react-src/src/routes/BodyRoute.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Body from '../body/Body';
+
+function BodyRoute() {
+    return <Body />
+}
+
+export default BodyRoute;
