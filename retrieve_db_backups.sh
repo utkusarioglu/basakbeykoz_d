b08a47c939355c6300acaeed647288c0525e348f
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-23 21:20:57 +0300

    Add script for retrieving sql backups

---
+++ retrieve_db_backups.sh
@@ -0,0 +1,9 @@
+PROJECT="basakbeykoz"
+ZONE="europe-west3-c"
+VM="basakbeykoz-7"
+DATE="test"
+VM_BACKUP_DIR=/mnt/disks/content_disk/backups/sql
+LOCAL_BACKUP_DIR=~/Do*/P*/Ba*/*d/backups/sql
+
+gcloud beta compute ssh --zone $ZONE $VM --project $PROJECT --command "docker exec basakbeykoz_db_p mysqldump -uroot -proot exampledb > $VM_BACKUP_DIR/$DATE.sql"
+gcloud compute scp --recurse utkusarioglu@basakbeykoz-7:$VM_BACKUP_DIR/* $LOCAL_BACKUP_DIR
