5ed2393203c6da79059f06bbad4abfeafc4f482a
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-25 15:32:46 +0300

    Fix var name typo

--- scripts/dev/retrieve_db_backups.sh
+++ scripts/dev/retrieve_db_backups.sh
@@ -6,5 +6,5 @@ DATE=$(date +'%Y%m%d')
 VM_SQL_BACKUP_DIR=/mnt/disks/content_disk/backups/sql
 LOCAL_SQL_BACKUP_DIR=~/Do*/P*/Ba*/*d/backups/sql
 
-gcloud beta compute ssh --zone $ZONE $VM --project $PROJECT --command "docker exec basakbeykoz_db_p mysqldump -uroot -proot exampledb > $VM_BACKUP_DIR/$DATE.sql"
+gcloud beta compute ssh --zone $ZONE $VM --project $PROJECT --command "docker exec basakbeykoz_db_p mysqldump -uroot -proot exampledb > $VM_SQL_BACKUP_DIR/$DATE.sql"
 gcloud compute scp --recurse --compress $USER@$VM:$VM_SQL_BACKUP_DIR/* $LOCAL_SQL_BACKUP_DIR
