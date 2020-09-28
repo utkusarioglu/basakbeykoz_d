06dd1e7d3a1be29c62068f24376d136befc41b9c
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-28 17:14:10 +0300

    Remove needless useEffect from Spinner

--- themes/basakbeykoz/react-src/src/features/spinner/Spinner.tsx
+++ themes/basakbeykoz/react-src/src/features/spinner/Spinner.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
-// import { CSSStyles } from "./@types-app";
 import { RootState } from "../../store/rootReducer";
 import SpinnerView from '../../components/spinner/SpinnerView';
 
@@ -17,31 +16,27 @@ type Props = DispatchProps & StateProps & OwnProps;
 
 function Spinner(props: Props): React.FunctionComponentElement<Props> {
 
-    // !TODO you use redux, useState shouldn't be here
-    const [display, setDisplay] = useState("none");
+    // !TODO you use redux, useState shouldn't be   
 
+    const { isFetching } = props;
     const loadingMessages = [
         "Hemen geliyor",
-        "Tazeden gelsin",
+        "Tazeden gelsin",           
         "Ailecek yukluyoruz",
         "Sayfa bizden, okumak sizden",
         "Aaa negezel yukluyo",
     ];
-
-    useEffect(() => {
-        if(props.isFetching) {
-            setDisplay("grid");
-        } else {
-            setDisplay("none");
-        }
-    }, [props.isFetching]);
 
     // selects a random message from loading messages array
     const loadingMessage = loadingMessages[
         Math.floor(Math.random()*(loadingMessages.length - 1))
     ] + "...";
 
-    return <SpinnerView display={display} loadingMessage={loadingMessage} />
+    return (
+        <SpinnerView 
+            display={isFetching ? 'grid' : 'none'} 
+            loadingMessage={loadingMessage} />
+    )
 }
 
 export default connect<StateProps, DispatchProps, OwnProps>(
