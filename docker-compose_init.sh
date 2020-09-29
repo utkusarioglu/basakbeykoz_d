954091de2bea324a473e8867d6b4cc98ddeb561b
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-29 10:12:04 +0300

    Fixes #9

--- docker-compose_init.sh
+++ docker-compose_init.sh
@@ -3,9 +3,9 @@
 bash ./scripts/dev/sync_with_production.sh
 wait
 docker-compose up -d
-sudo rm -rf ./themes/twenty* ./themes/index* 
-sudo rm -rf ./plugins/akis* ./plugins/index* ./plugins/hello*
 #sleep can later be replaced by a better command later
 sleep 20
+sudo rm -rf ./themes/twenty* ./themes/index* 
+sudo rm -rf ./plugins/akis* ./plugins/index* ./plugins/hello*
 bash ./scripts/basakbeykoz_db_d/host_init.sh
 (cd ./themes/basakbeykoz/react-src && sudo yarn start)
