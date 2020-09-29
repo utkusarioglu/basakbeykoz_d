11534f89a07404d0159265855e12dcbee91840a5
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-29 12:01:17 +0300

    Restyle footer
    Backdrop of the footer has been moved to its own div because of the
    graphic discrepancy caused by credits section receiving a border radius

--- themes/basakbeykoz/react-src/src/components/footer/FooterView.tsx
+++ themes/basakbeykoz/react-src/src/components/footer/FooterView.tsx
@@ -6,27 +6,34 @@ interface Props {
     navClickActions: () => void;
 }
 
-function FooterView(props: Props): React.FunctionComponentElement<Props> {
+function FooterView(props: Props) {
     const { REACT_APP_UPLOADS_DIR } = process.env;
-    const {navClickActions} = props;
+    const { navClickActions } = props;
     return (
         <div className="Footer">
-            <div className="Footer-art">
-                <img
-                    className="Footer-art-fg"
-                    alt="Footer decoration" 
-                    src={REACT_APP_UPLOADS_DIR + "/footer-art-fg.svg"} 
-                    />
-            </div>
-            <Link
-                onClick={navClickActions}
-                to="/credits">
-                <div className="Footer-credits">
-                    <span className="Footer-yearAndName">
-                        ♡<br />{ new Date(Date.now()).getFullYear()}<br />Basak Beykoz
-                    </span>
+            <div className="Footer-backdrop" />
+            <div className="Footer-elements">
+                <div className="Footer-elements-art">
+                    <img
+                        className="Footer-elements-art-fg"
+                        alt="Footer decoration" 
+                        src={REACT_APP_UPLOADS_DIR + "/footer-art-fg.svg"} 
+                        />
                 </div>
-            </Link>
+                <Link
+                    onClick={navClickActions}
+                    to="/credits">
+                    <div className="Footer-elements-credits">
+                        <span className="Footer-elements-credits-yearAndName">
+                            ♡
+                            <br />
+                            { new Date(Date.now()).getFullYear()}
+                            <br />
+                            Başak Beykoz
+                        </span>
+                    </div>
+                </Link>
+            </div>
         </div>
     )
 }
