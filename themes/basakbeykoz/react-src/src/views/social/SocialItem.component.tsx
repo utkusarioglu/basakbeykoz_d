e8aad4694df88f70ad4812922e3631aa0a9f4fc3
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-10-17 10:04:48 +0300

    Rename html class from Social-icon to Social-Item

--- themes/basakbeykoz/react-src/src/views/social/SocialItem.component.tsx
+++ themes/basakbeykoz/react-src/src/views/social/SocialItem.component.tsx
@@ -1,41 +1,43 @@
 import React from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { ISocialItem } from "../../features/social/@types-social";
-import { setIsMenuOpen } from "../../features/app/appActions";
+import { boundSetIsMenuOpen } from "../../features/app/appActions";
 import ReactGA from "react-ga";
 import { RootState } from "../../store/rootReducer";
 import "./_socialItem.view.scss";
 
 const mapState = (state: RootState) => ({});
 const mapDispatch = {
-  setIsMenuOpen,
+  setIsMenuOpen: boundSetIsMenuOpen,
 };
 const connector = connect(mapState, mapDispatch);
 type PropsFromRedux = ConnectedProps<typeof connector>;
-interface OwnProps {
-  item: ISocialItem;
-}
-
+type OwnProps = ISocialItem;
 type Props = OwnProps & PropsFromRedux;
 
 function SocialItem(props: Props) {
-  const { item, setIsMenuOpen } = props;
+  const { title, link, icon, setIsMenuOpen } = props;
 
   return (
     <ReactGA.OutboundLink
-      eventLabel={item.title}
-      onClick={() => setIsMenuOpen(false)}
-      to={item.link}
-      target="_blank"
+      {...{
+        eventLabel: title,
+        onClick: () => setIsMenuOpen(false),
+        to: link,
+        target: "_blank",
+      }}
     >
       <img
-        className="Social-icon"
-        src={item.icon}
-        title={item.title}
-        alt={item.title}
-      ></img>
+        {...{
+          className: "Social-item",
+          src: icon,
+          title,
+          alt: title,
+        }}
+      />
     </ReactGA.OutboundLink>
   );
 }
 
 export default connector(SocialItem);
+export type SocialItemOwnProps = OwnProps;
