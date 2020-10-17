f43f4ff58ed73f262849aa30d775ef8dec810995
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-10-17 09:41:18 +0300

    Add remove standard themes and plugins on ./*_up*
    Now the ./*_up* script removes the default plugins and the themes that
    ship with wordpress

--- docker-compose_up.sh
+++ docker-compose_up.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 docker-compose up -d
+sudo rm -rf ./themes/twenty* ./themes/index* 
+sudo rm -rf ./plugins/akis* ./plugins/index* ./plugins/hello*
 (cd ./themes/basakbeykoz/react-src && sudo yarn start)
