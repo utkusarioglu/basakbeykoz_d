28a68d1720ecd63a4160049ef87956d2353b5149
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-10-24 12:58:32 +0300

    Fix styling issues
    Fixes the <p> margin issue in the 4th child of the home hage.
    
    Fixes custom scrollbar styling issue with vertical scrollbars
    
    Adds enable/disable scroll section animation feature

--- themes/basakbeykoz/react-src/src/features/slugged/Slugged.feature.tsx
+++ themes/basakbeykoz/react-src/src/features/slugged/Slugged.feature.tsx
@@ -172,6 +172,7 @@ function getSlugOnload(
 function attachListActions(elem: HTMLElement): void {
   const SCROLL_DURATION = 1500;
   const LINGER_DURATION = 3000;
+  const ANIMATION_ENABLED = true;
 
   const scrollbarRef = OverlayScrollbars(elem, {
     scrollbars: {
@@ -182,28 +183,30 @@ function attachListActions(elem: HTMLElement): void {
   const children = osContent.children;
   const childrenCount = children.length;
   let currentChild = 1;
-  const animation = pauseable.setInterval(() => {
-    scrollbarRef.scroll(
-      {
-        el: children[currentChild] as HTMLElement,
-        margin: true,
-      },
-      SCROLL_DURATION,
-      'easeInOutSine'
-    );
-    // !TODO there is error in this, off by one
-    currentChild = (currentChild % childrenCount) + 1;
-  }, LINGER_DURATION);
+  if (ANIMATION_ENABLED) {
+    const animation = pauseable.setInterval(() => {
+      scrollbarRef.scroll(
+        {
+          el: children[currentChild] as HTMLElement,
+          margin: true,
+        },
+        SCROLL_DURATION,
+        'easeInOutSine'
+      );
+      // !TODO there is error in this, off by one
+      currentChild = (currentChild % childrenCount) + 1;
+    }, LINGER_DURATION);
 
-  const target = elem.querySelectorAll('.os-content')[0];
+    const target = elem.querySelectorAll('.os-content')[0];
 
-  target.addEventListener('mouseover', () => {
-    scrollbarRef.scrollStop();
-    animation.pause();
-  });
-  target.addEventListener('mouseleave', () => {
-    animation.resume();
-  });
+    target.addEventListener('mouseover', () => {
+      scrollbarRef.scrollStop();
+      animation.pause();
+    });
+    target.addEventListener('mouseleave', () => {
+      animation.resume();
+    });
+  }
 }
 
 export default connector(SluggedFeature);
