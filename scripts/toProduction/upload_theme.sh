2aaf0af0c4967e66bec577ef0dba6818d568cde2
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-10-30 19:03:45 +0300

    Update permission management for upload
    Upload.sh was giving a permission error as the files used by wordpress
    were altered to have the user www-data. Now the script file makes a
    prior connection to first alter the privileges of the files and then
    starts the upload

--- scripts/toProduction/upload_theme.sh
+++ scripts/toProduction/upload_theme.sh
@@ -9,4 +9,9 @@ REMOTE_THEME_DIR=$REMOTE_PROJECT_DIR/dist
 COMMAND="mkdir -p $REMOTE_THEME_DIR"
 gcloud compute ssh $USER@$VM --zone=$ZONE --command="$COMMAND"
 
+echo 'Altering permissions'
+gcloud compute ssh $USER@$VM --zone $ZONE --command "sudo chown -R $USER:$USER $REMOTE_THEME_DIR"
+echo 'Uploading theme files'
 gcloud compute scp --recurse --compress $LOCAL_THEME_DIR/* $USER@$VM:$REMOTE_THEME_DIR --zone $ZONE 
+# echo 'Reverting permissions'
+# gcloud compute ssh $USER@$VM --zone $ZONE --command "sudo chown -R www-data:www-data $REMOTE_THEME_DIR"
