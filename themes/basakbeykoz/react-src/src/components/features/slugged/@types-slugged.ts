f17238c54ba5cc4e2dabccda021935c9d06dc12d
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-12-06 17:42:34 +0300

    Feature: add UnmountFunction type, documentation

--- themes/basakbeykoz/react-src/src/components/features/slugged/@types-slugged.ts
+++ themes/basakbeykoz/react-src/src/components/features/slugged/@types-slugged.ts
@@ -1,6 +1,10 @@
 import { SluggedTimestampedSingular } from '../wordpress/@types-wordpress';
 
-export interface ISlugged {
+/**
+ * The store shape of items from wp that are called by their slugs, such
+ * as posts and pages
+ */
+export interface Slugged {
   post: {
     fetchTime: number;
     items: SluggedTimestampedSingular;
@@ -9,3 +13,15 @@ export interface ISlugged {
     items: SluggedTimestampedSingular;
   };
 }
+
+/**
+ * Defines the type of function that is executed to unmount the side effects of
+ * an injection
+ *
+ * @remarks
+ * The injections that may be made to the wordpress rendered pages may contain
+ * some event listeners and other items that may need to be removed. Unmount
+ * functions that are returned from the injections do these. This type defines
+ * their shape
+ */
+export type UnmountFunction = () => void;
